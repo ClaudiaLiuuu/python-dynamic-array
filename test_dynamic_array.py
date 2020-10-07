41902d316a418b40da94ef79d75f5cd144104af1
Aidan Baack <aebaack@gmail.com>
2020-10-07 10:17:50 -0600

    Setup skeleton for testing

---
+++ test_dynamic_array.py
@@ -0,0 +1,41 @@
+import unittest
+from dynamic_array import DynamicArray
+
+
+class DynamicArrayTestCase(unittest.TestCase):
+    def test_append(self):
+        self.assertEqual(True, False)
+
+    def test_extend(self):
+        self.assertEqual(True, False)
+
+    def test_insert(self):
+        self.assertEqual(True, False)
+
+    def test_remove(self):
+        self.assertEqual(True, False)
+
+    def test_pop(self):
+        self.assertEqual(True, False)
+
+    def test_clear(self):
+        self.assertEqual(True, False)
+
+    def test_index(self):
+        self.assertEqual(True, False)
+
+    def test_count(self):
+        self.assertEqual(True, False)
+
+    def test_sort(self):
+        self.assertEqual(True, False)
+
+    def test_reverse(self):
+        self.assertEqual(True, False)
+
+    def test_copy(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
