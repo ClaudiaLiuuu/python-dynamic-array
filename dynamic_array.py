8e9ca2da4c0f1bb8ada02103b7cfa5a3f076f473
Aidan Baack <aebaack@gmail.com>
2020-10-07 13:25:07 -0600

    Add helper methods for shrinking and growing the array

--- dynamic_array.py
+++ dynamic_array.py
@@ -6,6 +6,7 @@ class DynamicArray:
         self._length = 0
         self._capacity = 1
         self._arr = self._create_array(self._capacity)
+        self._growth_factor = 2
 
     def __getitem__(self, idx):
         """Return the element at the specified index"""
@@ -20,7 +21,7 @@ class DynamicArray:
     def append(self, element):
         """Add a new element to the end of the array"""
         if self._length == self._capacity:  # Need to increase size
-            self._resize_arr(self._capacity * 2)
+            self._grow_arr()
 
         self._arr[self._length] = element
         self._length += 1
@@ -30,7 +31,18 @@ class DynamicArray:
         for element in seq:
             self.append(element)
 
+    def _shrink_arr(self):
+        """Increase the capacity of the array by current capacity * growth factor"""
+        self._resize_arr(self._capacity // self._growth_factor)
+
+    def _grow_arr(self):
+        """Decrease the capacity of the array by current capacity / growth factor"""
+        self._resize_arr(self._capacity * self._growth_factor)
+
     def _resize_arr(self, new_capacity):
+        """Resize the array to the specified capacity"""
+        if new_capacity < self._length:
+            raise RuntimeError('New capacity is lower than length')
         longer_arr = self._create_array(new_capacity)
         for i in range(self._length):
             longer_arr[i] = self._arr[i]
