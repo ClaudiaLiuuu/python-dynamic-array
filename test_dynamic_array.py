cd8f0c330cb2c5ed8d41bb2e4587198f2baac4ef
Aidan Baack <aebaack@gmail.com>
2020-10-07 11:24:06 -0600

    Test append functionality and amortized runtime

--- test_dynamic_array.py
+++ test_dynamic_array.py
@@ -1,10 +1,39 @@
 import unittest
+from time import time
 from dynamic_array import DynamicArray
 
 
 class DynamicArrayTestCase(unittest.TestCase):
+    @staticmethod
+    def time_array_appends(num_elements):
+        """Returns average time in seconds of num_elements appends"""
+        arr = DynamicArray()
+        start_time = time()
+        for i in range(num_elements):
+            arr.append(i)
+        end_time = time()
+        return (end_time - start_time) / num_elements
+
+    def setUp(self):
+        self.arr = DynamicArray()
+
     def test_append(self):
-        self.assertEqual(True, False)
+        """Test correct placement in array when appending 1,000 numbers"""
+        num_elements = 1000
+        self.assertEqual(len(self.arr), 0)
+        for i in range(num_elements):
+            self.arr.append(i)
+            self.assertEqual(self.arr[i], i)
+        self.assertEqual(len(self.arr), num_elements)
+
+    def test_append_performance(self):
+        """Test amortized runtime by comparing 1,000 appends with 1,000,000"""
+        avg_small = self.time_array_appends(10)
+        avg_medium = self.time_array_appends(1000)
+        avg_large = self.time_array_appends(1000000)
+        self.assertAlmostEqual(avg_small, avg_medium, delta=0.05)
+        self.assertAlmostEqual(avg_small, avg_large, delta=0.05)
+        self.assertAlmostEqual(avg_medium, avg_large, delta=0.05)
 
     def test_extend(self):
         self.assertEqual(True, False)
