391030606ef657de015d95f24b218045d2d6e6d2
Aidan Baack <aebaack@gmail.com>
2020-10-10 09:19:07 -0600

    Generalize method testing by finding method as attribute

--- runtime_comparison.py
+++ runtime_comparison.py
@@ -2,21 +2,24 @@ from dynamic_array import DynamicArray
 from time import time
 
 
-def test_append(size, value):
+def test_method(method, size, value=None):
     arr_dyn = DynamicArray()
     arr_list = list()
-    start_time = time()
-    for i in range(size):
-        arr_dyn.append(value)
-    end_time = time()
-    avg_dyn = ((end_time - start_time) / size) * 10**6
-    start_time = time()
-    for i in range(size):
-        arr_list.append(value)
-    end_time = time()
-    avg_list = ((end_time - start_time) / size) * 10**6
+    func1 = arr_dyn.__getattribute__(method)
+    func2 = arr_list.__getattribute__(method)
+
+    def time_method(func):
+        start_time = time()
+        for i in range(size):
+            func(value)
+        end_time = time()
+        return ((end_time - start_time) / size) * 10 ** 6
+
+    avg_dyn = time_method(func1)
+    avg_list = time_method(func2)
+
     return avg_dyn, avg_list
 
 
-avg_dyn_append, avg_list_append = test_append(1000000, 1)
+avg_dyn_append, avg_list_append = test_method('append', 1000000, 1)
 print(avg_dyn_append, avg_list_append)
