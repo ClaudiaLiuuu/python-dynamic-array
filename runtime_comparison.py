8fbb801af51244ad27b08bfe098d80eb43ed73a1
Aidan Baack <aebaack@gmail.com>
2020-10-10 09:35:04 -0600

    Create method for formatting results of comparison

--- runtime_comparison.py
+++ runtime_comparison.py
@@ -2,24 +2,37 @@ from dynamic_array import DynamicArray
 from time import time
 
 
-def test_method(method, size, value=None):
+def test_method_runtime(method, size, value=None):
+    """Return average runtime of method on size elements for native list and DynamicArray"""
     arr_dyn = DynamicArray()
     arr_list = list()
-    func1 = arr_dyn.__getattribute__(method)
-    func2 = arr_list.__getattribute__(method)
+
+    # Get alias for respective method in class
+    method_dyn = arr_dyn.__getattribute__(method)
+    method_list = arr_list.__getattribute__(method)
 
     def time_method(func):
-        start_time = time()
-        for i in range(size):
+        """Return average runtime in microseconds of function over size elements"""
+        start_time = time()  # Begin timing
+        for i in range(size):  # Run size operations
             func(value)
-        end_time = time()
-        return ((end_time - start_time) / size) * 10 ** 6
+        end_time = time()  # End timing
+        return ((end_time - start_time) / size) * 10 ** 6  # Return average
 
-    avg_dyn = time_method(func1)
-    avg_list = time_method(func2)
-
-    return avg_dyn, avg_list
+    return time_method(method_dyn), time_method(method_list)
 
 
-avg_dyn_append, avg_list_append = test_method('append', 1000000, 1)
-print(avg_dyn_append, avg_list_append)
+def format_method(method_name, value=None):
+    dyn_sml, nat_sml = test_method_runtime(method_name, 100, value)
+    dyn_med, nat_med = test_method_runtime(method_name, 10000, value)
+    dyn_lrg, nat_lrg = test_method_runtime(method_name, 1000000, value)
+
+    results[method_name] = {
+        'dynamic': [dyn_sml, dyn_med, dyn_lrg],
+        'native': [nat_sml, nat_med, nat_lrg],
+    }
+
+
+results = {}
+format_method('append', 5)
+print(results)
